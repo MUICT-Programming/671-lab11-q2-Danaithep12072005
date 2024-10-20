9f7de119de002122d2d1bf1b4d41c04232cb8c86
Danaithep12072005 <man007659558@gmail.com>
2024-10-20 15:23:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# YOUR CODE HERE
+lst1 = []
+lst2 = []
+updated_list = []
+def summation(n):
+    for i in range(n):
+        x = int(input())
+        lst1.append(x)
+    for i in range(n):
+        z = int(input())
+        lst2.append(z)
+        updated_list.append(lst1[i]+lst2[i])
+    return updated_list
+def find_min_max(updated_list):
+    nummin = min(updated_list)
+    nummax = max(updated_list)
+    return nummin,nummax
+print(summation(n))
+print(find_min_max(updated_list))UR CODE HERE
